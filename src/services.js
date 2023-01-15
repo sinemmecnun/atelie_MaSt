8dc75f56a54fba4ac776a06f7eec0721ccf55952
sinemmecnun <sinemmecnun@gmail.com>
2023-01-15 21:33:09 +0200

    finalized the project

--- src/services.js
+++ src/services.js
@@ -4,7 +4,8 @@ const sizeButton = document.getElementById('size-button');
 const priceBtn = document.getElementById('price-button');
 const orderButton = document.getElementById('show-order');
 const rentButton = document.getElementById('show-rent');
-const questionButtons = document.querySelectorAll('.send-question');
+const sendOrder = document.getElementById('send-order');
+const sendRent = document.getElementById('send-rent');
 
 const sizeDiv = document.getElementById('womens-sizes-div');
 const priceDiv = document.getElementById('price-list');
@@ -27,4 +28,33 @@ rentButton.addEventListener('click', () =>{
     expandNew(rentDiv);
 });
 
+sendOrder.addEventListener('click', (event) => {
+    event.preventDefault();
+    const name = document.getElementById('order-name').value;
+    const phone = document.getElementById('order-phone').value;
+    const email = document.getElementById('order-email').value;
+    const message = document.getElementById('order-message').value;
+    
+    if(name == '' || phone == '' || email == '' || message == ''){
+        return alert('Всички полета са задължителни!');
+    }
 
+    const parent = sendOrder.parentElement.parentElement;
+    document.getElementById('order-message-sent').style.display = 'flex';
+    parent.style.display = 'none';
+})
+
+sendRent.addEventListener('click', (event) => {
+    const name = document.getElementById('rent-name').value;
+    const phone = document.getElementById('rent-phone').value;
+    const email = document.getElementById('rent-email').value;
+    const message = document.getElementById('rent-message').value;
+    
+    if(name == '' || phone == '' || email == '' || message == ''){
+        return alert('Всички полета са задължителни!');
+    }    
+    // location.reload();
+    const parent = sendRent.parentElement.parentElement;
+    document.getElementById('rent-message-sent').style.display = 'flex';
+    parent.style.display = 'none';
+})
