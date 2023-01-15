01661ae4bd3b8603333f66706c12b64b4cbc02a0
sinemmecnun <sinemmecnun@gmail.com>
2023-01-15 20:33:45 +0200

    fixed buttons

--- src/catalogueItems.js
+++ src/catalogueItems.js
@@ -79,7 +79,7 @@ export function populateDetails(id, type){
 
 export function expand(div){
     if(div.style.display == 'none'){
-        div.style.display = 'block';
+        div.style.display = 'grid';
     }
     else{
         div.style.display = 'none';
